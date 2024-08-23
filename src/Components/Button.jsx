f98a4b4941acd7ee9a7848e9f36a542e87e6811f
AbdullahMukadam <104569989+AbdullahMukadam@users.noreply.github.com>
2024-08-23 22:02:22 +0530

    Update Button.jsx

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -6,6 +6,7 @@ function Button({
     className="",
     ...props
 }) {
+    // Common Btn
   return (
     <button type={type} className={`px-3 py-2 rounded-md bg-blue-500 text-black ${className}`} {...props}>{children}</button>
   )
